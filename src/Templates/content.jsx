72d924d73b7f896266c96cbd798b36a55dd69c1a
Leonardo Nava (@L3oNav) <L3oNav@outlook.com>
2019-08-19 16:25:28 -0700

    responsive design, but not added a responsive design for phones

--- src/Templates/content.jsx
+++ src/Templates/content.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import Biography from '../Components/Biography.jsx'
-import Portfolio from '../Components/Projects.jsx'
+import React, { Component } from "react";
+import Biography from "../Components/Biography.jsx";
+import Portfolio from "../Components/Projects.jsx";
 class Content extends Component {
-    // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div className="grid-content">
-                <h3 className="text-about">About Me</h3>
-                <div className="line-h"></div>
-                <div className="container-content">
-                    <div className="container">
-                        <Biography />
-                    </div>
-                </div>
-                <div className="portfolio-container">
-                    <Portfolio/>
-                </div>
-            </div>
-        );
-    }
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="grid-content">
+        <h3 className="text-about">About Me</h3>
+        <div className="line-h" />
+        <div className="container-content">
+          <div className="container">
+            <Biography />
+          </div>
+        </div>
+        <div className="portfolio-container">
+          <Portfolio />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Content;
