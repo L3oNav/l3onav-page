617984b0bb245d0736309cd881e68dd236ba1f92
Leonardo Nava <L3oNav@outlook.com>
2020-01-03 16:16:11 -0800

    Header modified like now.sh

--- src/Templates/Header.jsx
+++ src/Templates/Header.jsx
@@ -1,36 +1,41 @@
-/* eslint-disable jsx-a11y/alt-text */
-import React from 'react';
-import UserInf from '../Components/UserInf.jsx'
-import FontImg from '../Images/fondoUserInfo.jpg'
-import UserImg from '../Images/UserImg.jpeg'
-import LangSelector from '../Components/LangSelector.jsx'
+import React from "react";
+import UserInf from "../Components/UserInf.jsx";
+import FontImg from "../Images/fondoUserInfo.jpg";
+import UserImg from "../Images/UserImg.jpeg";
+import LangSelector from "../Components/LangSelector.jsx";
+import { connect } from "react-redux";
+import { getLanguage } from "../Redux/actions/index";
 
 const User = {
-    img: UserImg,
-    name:'Leonardo Nava',
-    twitter:'https://platzi.com/@L3oNav/',
-    username:'@L3oNav',
-    job:'Web Developer Reactjs',
-}
+	img: UserImg,
+	name: "Leonardo Nava",
+	username: "@L3oNav",
+};
 
-const Header = () => {
-  return (
-    <div className="grid-info">
-      <div>
-        <LangSelector/>
-      </div>
-      <div className="overflow">
-        <img src={FontImg} className="font-img" />
-      </div>
-      <div className="header-inf">
-        <UserInf
-          UsrImg={User.img}
-          UsrName={User.name}
-          UsrJob={User.job}
-        />
-      </div>
-    </div>
-  );
-}
-
-export default Header;
+const Header = ({ language }) => {
+	getLanguage();
+	return (
+		<div className='grid-info'>
+			<div>
+				<LangSelector />
+			</div>
+			<div className='overflow'>
+				<img src={FontImg} className='font-img' />
+			</div>
+			<div className='header-inf'>
+				{language ? (
+					<UserInf UsrImg={User.img} UsrName={User.name} UsrJob={language.job} />
+				) : null}
+			</div>
+		</div>
+	);
+};
+const mapStateToProps = (state) => {
+	return {
+		language: state.language,
+	};
+};
+const mapDispatchToProps = {
+	getLanguage,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
