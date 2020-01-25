26c191b17af5d8374ca49ede49c7676dd537e2b6
Leonardo Nava <L3oNav@outlook.com>
2020-01-24 17:07:35 -0800

    added files por portafolio area

--- src/Components/UserInf.jsx
+++ src/Components/UserInf.jsx
@@ -10,6 +10,7 @@ function UserInf(props) {
             <h1>{props.UsrName}</h1>
             <h3>{props.UsrJob}</h3>
           </a>
+            <h3 style={{marginLeft:'70px'}}>click &#8593;</h3>
         </div>
       </div>
     </div>
