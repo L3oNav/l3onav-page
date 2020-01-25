02c36a92cca719faa5eafe140db69ceff25467cf
Leonardo Nava <L3oNav@outlook.com>
2020-01-24 17:03:28 -0800

    added job in header like now.sh

--- src/Templates/Header.jsx
+++ src/Templates/Header.jsx
@@ -1,41 +1,53 @@
-import React from "react";
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Component } from "react";
 import UserInf from "../Components/UserInf.jsx";
 import FontImg from "../Images/fondoUserInfo.jpg";
 import UserImg from "../Images/UserImg.jpeg";
 import LangSelector from "../Components/LangSelector.jsx";
-import { connect } from "react-redux";
 import { getLanguage } from "../Redux/actions/index";
+import { connect } from "react-redux";
 
 const User = {
 	img: UserImg,
 	name: "Leonardo Nava",
-	username: "@L3oNav",
+	twitter: "https://platzi.com/@L3oNav/",
 };
 
-const Header = ({ language }) => {
-	getLanguage();
-	return (
-		<div className='grid-info'>
-			<div>
-				<LangSelector />
-			</div>
-			<div className='overflow'>
-				<img src={FontImg} className='font-img' />
-			</div>
-			<div className='header-inf'>
-				{language ? (
-					<UserInf UsrImg={User.img} UsrName={User.name} UsrJob={language.job} />
-				) : null}
-			</div>
-		</div>
-	);
-};
+class Header extends Component {
+	componentDidMount() {
+		if (!this.props.language) {
+			this.props.getLanguage();
+		}
+	}
+	render() {
+		const { language } = this.props;
+		return (
+				<div className='grid-info'>
+					<div>
+						<LangSelector />
+					</div>
+					<div className='overflow'>
+						<img src={FontImg} className='font-img' />
+					</div>
+					<div className='header-inf'>
+						{language? 
+							<UserInf UsrImg={User.img} UsrName={User.name} UsrJob={language.job} />
+							: null
+						}
+					</div>
+				</div>
+		)
+	}
+}
+
 const mapStateToProps = (state) => {
-	return {
-		language: state.language,
-	};
+  return { 
+    language: state.language,
+  }
 };
 const mapDispatchToProps = {
 	getLanguage,
 };
-export default connect(mapStateToProps, mapDispatchToProps)(Header)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
+
