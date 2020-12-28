114b845031a2ebfa2e123ee5d3ff74643a970f3f
Leonardo Nava (L3oNav) <L3oNav@outlook.com>
2020-12-27 21:20:21 -0800

    change message of me

--- src/Redux/Reducers/languages.js
+++ src/Redux/Reducers/languages.js
@@ -2,9 +2,9 @@ export const Languages = {
 	ES: {
 		name: 'ES',
 		job: 'Desarrollador Web con Reactjs y Django.',
-		title: 'Hola Mundo!',
+		title: 'Hola, Mundo!',
 		biography:
-			'Soy Leonardo Nava, estudiaré física, me gustaría ser un científico de datos. Me gusta aprender cosas nuevas todos los días, así es como aprendí a programar, aprendiendo solo, después de unos meses descubrí platzi donde realmente fue un cambio en mi aprendizaje. Soy alguien a quien le gusta leer mucho, leo fantasía épica, matemáticas, física, hago mi mejor esfuerzo para mejorar día a día.',
+			'Me llamo Leonardo Nava, estudiaré física, me gustaría ser un científico de datos. Me gusta aprender cosas nuevas todos los días, así es como aprendí a programar, aprendiendo solo, después de unos meses descubrí platzi donde realmente fue un cambio en mi aprendizaje. Soy alguien a quien le gusta leer mucho, leo fantasía épica, matemáticas, física, hago mi mejor esfuerzo para mejorar día a día.',
 		buttons: [
 			// { id: 1, title: 'Inicio', to: '/' },
 			// { id: 2, title: 'Certificados', to: '/certificates' },
@@ -14,9 +14,9 @@ export const Languages = {
 	US: {
 		name: 'US',
 		job: 'Web app developer with ReactJs and Django.',
-		title: 'Hello, World',
-		biography:
-			'I am Leonardo Nava, I will study physics, I would like to be a data scientist. I like learning new things every day, this is how I learned to program, learning alone, after a few months I discovered platzi where it really was a change in my learning. I am someone who likes to read a lot, I read epic fantasy, math, physics, I do my best to improve day by day.',
+		title: 'Hello, World!',
+		biography:  
+'My name is Leonardo Nava, I will study physics and I would like to be a data scientist. I like to learn things every day which is why  I learn to program all by myself. After a few months  I discovered platzi, where there was a change in my learning. I am someone who likes to read a lot, especially epic fantasy, math and physics, I do my best to improve day by day.',
 		buttons: [
 			// { id: 1, title: 'Home', to: '/' },
 			// { id: 2, title: 'Certificates', to: '/certificates' },
