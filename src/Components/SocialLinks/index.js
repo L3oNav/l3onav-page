e6a83fde32177fb56024f103ef96c3d4f7abd20d
Leonardo <L3oNav@outlook.com>
2021-01-16 18:59:32 -0800

    added alts for images

--- src/Components/SocialLinks/index.js
+++ src/Components/SocialLinks/index.js
@@ -12,30 +12,37 @@ import { Container, Articles, Grid } from './styles';
 const social = [
 	{
 		id: 1,
+		alt: 'instagram',
 		component: FiInstagram,
 		url: 'https://www.instagram.com/l3onav/?hl=es-la'
 	},
 	{
+		id: 2,
+		alt: 'linkedin',
 		component: FiLinkedin,
 		url: 'https://www.linkedin.com/in/leonardo-nava-6015b3160'
 	},
 	{
 		id: 3,
+		alt: 'twitter',
 		component: FiTwitter,
 		url: 'https://twitter.com/L3oNav'
 	},
 	{
 		id: 4,
+		alt: 'github',
 		component: FiGithub,
 		url: 'https://github.com/L3oNav'
 	},
 	{
 		id: 5,
+		alt: 'email',
 		component: FiMail,
 		url: 'mailto:info@l3onav.tech?'
 	},
 	{
 		id: 6,
+		alt: 'gitlab',
 		component: FiGitlab,
 		url: 'https://gitlab.com/L3oNav/l3onav-portfolio'
 	}
@@ -47,7 +54,7 @@ class SocialLinks extends Component {
 			<Container>
 				<Grid>
 					{social.map(link => (
-							<Articles key={link.id} href={link.url} target='_blank' ref="noopener">
+							<Articles key={link.id} href={link.url} target='_blank' ref="noopener" alt={link.alt}>
 								<link.component size='35' color='white' />
 							</Articles>
 						)
