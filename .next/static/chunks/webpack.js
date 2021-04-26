538561f2a8f7c85c752080559969e90beec7e2e8
Leonardo Nava (L3oNav) <l3onav@outlook.com>
2021-04-25 19:13:49 -0700

    bug fix margin

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7104875f6a542f94107f";
+/******/ 	var hotCurrentHash = "f9afc14459093ec0c866";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
